54c82774ff9c8af9148a6c9dc6c3341bfb4ad3c9
Tyler Gray <tygray@uw.edu>
2017-03-20 07:42:36 -0700

    Homework 2 in Progress
    Homework 2 in Progress

--- exercises.js
+++ exercises.js
@@ -1,35 +1,71 @@
 //Do not change any of the function names
 
 function getBiggest(x, y) {
+  if (x > y) {
+  return x;
+    } else {
+      return y;
+    }
+  
   //x and y are integers.  Return the larger integer
   //if they are the same return either one
 }
 
 function greeting(language) {
+if (language === 'German') {
+  return 'Guten Tag!';}
+  else if (language === 'Spanish') {
+    return 'Hola!';
+  } else {
+    return 'Hello!';
+  }
+}
   //return a greeting for three different languages:
   //language: 'German' -> 'Guten Tag!'
   //language: 'English' -> 'Hello!'
   //language: 'Spanish' -> 'Hola!'
   //if language is undefined return 'Hello!'
-}
+
 
 function isTenOrFive(num) {
   //return true if num is 10 or 5
   //otherwise return false
+  if (num === 10 || num === 5) {
+    return true;
+  } else { 
+    return false;
+  }
 }
 
 function isInRange(num) {
-  //return true if num is less than 50 and greater than 20
+    if (num < 50 && num > 20) {
+    return true;
+  } else { 
+    return false;
+  }
 }
 
+  //return true if num is less than 50 and greater than 20
+
+
 function isInteger(num) {
+
+  if (num === Math.floor (num)) {
+
+    return true;
+
+  }
+
+  return false;
+
+}
   //return true if num is an integer
   //0.8 -> false
   //1 -> true
   //-10 -> true
   //otherwise return false
   //hint: you can solve this using Math.floor
-}
+
 
 function fizzBuzz(num) {
   //if num is divisible by 3 return 'fizz'
